d7398729b956128428c5090faa7f655443b1baf0
Deepak Bhamla <dbhamla7@gmail.com>
2020-06-21 19:30:34 +0530

    'chatWithAlex'

---
+++ chatWithAlex/chatWithAlex.py
@@ -0,0 +1,121 @@
+# ai using print and input only
+print('\n\t\tWelcome to CHAT WITH ALEX\n')
+print(' user Detail\n ')
+username,  password = input('username : '), input('password : ')
+user_hair = input(''' your Hair : ''')
+user_eyes = input(''' your Eyes : ''')
+user_mouth = input(''' your Mouth : ''')
+print('\n\tprofile setup successfully :\n ')
+print('\t\tCHAT WITH ALEX')
+
+print(f'''
+						$$$
+						- -    	
+						 -
+		    		       hello i am Alex
+''')
+print(f''' 
+     {user_hair}
+     {user_eyes}
+      {user_mouth}
+''')
+user_name = input(' .. ')
+print(f'''
+						$$$
+						- -    	
+						 -
+	                          hello {user_name.capitalize()}!!!
+                              welcome to chat with alex     
+
+''')
+print(f''' 
+     {user_hair}
+     {user_eyes}
+      {user_mouth}
+''')
+thanks = input('..')
+print(f'''
+						$$$
+						- -    	
+						 -
+                           I can calculate number  Very fast     
+                           and You .. 
+
+''')
+print(f''' 
+     {user_hair}
+     {user_eyes}
+      {user_mouth}
+''')
+quiz1 = input('..')
+print(f'''
+						$$$
+						- -    	
+						 -
+                           {quiz1.upper()}
+                           Wow its wonderful
+''')
+print(f'''
+						$$$
+						- -    	
+						 -
+                           I am looking like Iron Man   
+                           and You .. 
+
+''')
+print(f''' 
+     {user_hair}
+     {user_eyes}
+      {user_mouth}
+''')
+quiz2 = input('..')
+print(f'''
+						$$$
+						- -    	
+						 -
+                           {quiz2.upper()}
+                           O really....
+''')
+print(f'''
+						$$$
+						- -    	
+						 -
+                           So tell me your secret 
+                           i promised it will destroy after chat end
+''')
+print(f''' 
+     {user_hair}
+     {user_eyes}
+      {user_mouth}
+''')
+quiz3 = input('..')
+print(f'''
+						$$$
+						- -    	
+						 -
+                           Thanks to belive me
+''')
+
+print(f'''
+						$$$
+						- -    	
+						 -
+                        What I know About you..
+                        username : {username}
+                        password : {password}
+                        your name : {user_name} 
+                        profile pic :    
+                                          {user_hair}
+                                          {user_eyes}
+                                           {user_mouth}
+
+                        your secret : {quiz3.upper()}  
+                        PRESS ANY KEY TO DESTROY ME             
+''')
+print(f''' 
+     {user_hair}
+     {user_eyes}
+      {user_mouth}
+''')
+quiz4 = input('..')
+
